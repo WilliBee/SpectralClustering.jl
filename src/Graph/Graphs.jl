c5712f4c8b2c256974673acc4880c35a1ee05a10
WilliBee <cbeng.william@gmail.com>
2026-01-14 13:01:48 +0100

    Drop LightGraphs.jl and use Graphs.jl

--- src/Graph/Graphs.jl
+++ src/Graph/Graphs.jl
@@ -11,9 +11,9 @@ export
        cycles,
     number_of_neighbors
 
-using LightGraphs
+using Graphs
 
-import LightGraphs: nv, ne, has_edge, is_directed
+import Graphs: nv, ne, has_edge, is_directed
 
 import Base.length
 import Base.empty!
@@ -24,7 +24,7 @@ type Edge
 ```
 
 """
-mutable struct Edge{T}
+mutable struct Edge{T} <: Graphs.AbstractEdge{Int}
   next_v1::Union{Nothing,Edge{T}}
   prev_v1::Union{Nothing,Edge{T}}
   next_v2::Union{Nothing,Edge{T}}
@@ -54,7 +54,7 @@ end
 function Edge(v1::Vertex,v2::Vertex,w::Number)
     return Edge{weight_type(v1)}(nothing,nothing,nothing,nothing,v1,v2,convert(weight_type(v1),w))
 end
-struct Graph
+struct Graph <: Graphs.AbstractGraph{Int}
   vertices::Vector{Vertex}
   is_dirty::Bool
 end
